ce5b1f4983b30c2459d78421de894bdbcc3e88a7
JsMelix <juansmelo03@gmail.com>
2025-02-24 14:45:24 -0500

    addpage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import astroIcon from 'astro-icon'; // Importa astro-icon
+
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [astroIcon()], // Agrega astro-icon a integrations
+  site: 'https://JsMelix.github.io/seb-apps-wastro/',
+  base: '/seb-apps-wastro/',
 
 });
