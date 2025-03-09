950cabd3ab0682f6189b551abba8c1c08a68509d
JsMelix <juansmelo03@gmail.com>
2025-03-08 22:34:22 -0500

    addchanges

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://JsMelix.github.io/seb-apps-wastro/',
-  base: '/seb-apps-wastro/',
-
+  // Your config here
 });
